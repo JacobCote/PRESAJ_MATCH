cb347a2d341b9fa09fb38c150e1c8344b8f20213
Jacob cote <jacobcote@Air-de-Jacob.hitronhub.home>
2024-06-30 14:28:19 -0400

    add setup

--- setup.sh
+++ setup.sh
@@ -7,11 +7,12 @@ if [ ! -d "data" ]; then
 fi
 
 # Create a virtual environment if it doesn't exist
-if [ ! -d ".venv" ]; then
+if [ ! -d "venv" ]; then
+  echon 'venv not existing, crating a venv'
   # Create the virtual environment
-  python3 -m venv .venv
+  python3 -m venv venv
   # Activate the virtual environment
-  source .venv/bin/activate
+  source venv/bin/activate
   # Install dependencies from requirements.txt
   pip install -r requirements.txt
   # Deactivate the virtual environment
@@ -19,7 +20,7 @@ if [ ! -d ".venv" ]; then
 fi
 
 # Activate the virtual environment
-source .venv/bin/activate
+source venv/bin/activate
 # Run the Python script 'match.py'
 python3 match.py
 
